6ddd32517f7870d263b4964660db99790b99cae8
Nan Chen <itisnanful@gmail.com>
2014-11-14 14:24:47 -0800

    memmanager name changed to memorymanager

---
+++ code/userprog/memorymanager.h
@@ -0,0 +1,36 @@
+#ifndef MEMORYMANAGER_H
+#define MEMORYMANAGER_H
+
+#include "synch.h"
+#include "bitmap.h"
+#include "addrspace.h"
+
+class Record
+{
+ public:
+    bool allocated;
+    AddrSpace* space;
+    int vPageNumber;
+    bool ioLocked;
+};
+
+class MemoryManager
+{
+ public:
+    static MemoryManager* GetInstance();
+    ~MemoryManager();
+    int AllocPage();
+    void FreePage(int physPageNum);
+    bool PageIsAllocated(int physPageNum);
+    Record* entries;
+    int replaceIndex;
+    int totalPages;
+ private:
+    MemoryManager(int numPages);
+    BitMap* pages;
+    int usedPages;
+    Lock* mmLock;
+    static MemoryManager* manager;
+};
+
+#endif
