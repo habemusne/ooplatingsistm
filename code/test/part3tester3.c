740339399d9e1c31b5c62a02bd0fed894c746540
Nan Chen <nac001@ieng6-201.ucsd.edu>
2014-12-10 23:02:01 -0800

    sS

---
+++ code/test/part3tester3.c
@@ -0,0 +1,48 @@
+/*
+* "part3Tester2.c
+*
+* A test program that first reverse a sorted array, and then
+* reverse back. This tester can verify the functionality of 
+* the backing store and good locality.
+*
+*  If running successfully, the exit return value would be 0.
+*/
+
+#include "syscall.h"
+
+int A[1000];
+
+int
+main()
+{
+  int i, j, tmp;
+
+  for (i = 0; i < 1000; i++) {
+    A[i] = 1000 - i;
+  }
+  for (i = 0; i < 1000; i++) {
+    for (j = 0; j < i; j++) {
+      if (A[i] < A[j]) { 
+        tmp = A[j];
+        A[j] = A[i];
+        A[i] = tmp;
+      }
+    }
+  }
+
+  for (i = 0; i < 1000; i++) {
+    A[i] = 1000 - i;
+  }
+
+  for (i = 0; i < 1000; i++) {
+    for (j = 0; j < i; j++) {
+      if (A[i] < A[j]) { 
+        tmp = A[j];
+        A[j] = A[i];
+        A[i] = tmp;
+      }
+    }
+  }
+  
+  Exit(A[0]);    /* and then we're done -- should be 0! */
+}
